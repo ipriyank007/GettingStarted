9ca154cc46467b8108411b7405e96e7af0b49040
Priyank <ipriyank007@hotmail.com>
2019-06-28 18:04:18 +0530

    Update requestInPython.py

--- PythonBatch-(M-P-N-K-A-R)/requestInPython.py
+++ PythonBatch-(M-P-N-K-A-R)/requestInPython.py
@@ -1,12 +1,18 @@
 import requests
 
-para = {'username': 'priyank', 'password': 'password123'}
-r = requests.post('https:/httpbin.org/basic-auth/', data=para)
+r = requests.get('https://www.xkcd.com/353')
 
-# with open('Python_Comics.png', 'wb') as f:
-#     f.write(r.content)
+#para = {'username': 'priyank', 'password': 'password123'}
+#r = requests.post('https:/httpbin.org/basic-auth/', data=para)
+
+#print(r.get) #will take in string format)
+#print(r.content) #will take in bytes
+
+
+with open('Python_Comics.png', 'wb') as f:
+    f.write(r.content)
 # print(r.ok)
 # print(r.status_code)
 # r_dict = r.json()
 # print(r_dict)
-print(r.json())
+#print(r.json())
