50e8393bd9858c7f28ccfeb8346c45af5bc62049
Priyank <ipriyank007@hotmail.com>
2019-07-01 11:26:43 +0530

    Update calc_for_test.py

--- PythonBatch-(M-P-N-K-A-R)/testing_unittest/calc_for_test.py
+++ PythonBatch-(M-P-N-K-A-R)/testing_unittest/calc_for_test.py
@@ -1,16 +1,18 @@
-def add(a, b):
-    return a + b
+class Calc:
+    s
+    def add(a, b):
+        return a + b
 
 
-def subtract(a, b):
-    return a - b
+    def subtract(a, b):
+        return a - b
 
 
-def multiply(a, b):
-    return a * b
+    def multiply(a, b):
+        return a * b
 
 
-def divide(a, b):
-    if b == 0:
-        raise ValueError("You cannot divide nums by zero!")
-    return a / b
+    def divide(a, b):
+        if b == 0:
+            raise ValueError("You cannot divide nums by zero!")
+        return a / b
