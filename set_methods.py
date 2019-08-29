87eacf7db03bc77b59dc439029440fd16a5a360f
Priyank <ipriyank007@hotmail.com>
2019-08-29 16:37:03 +0530

    Add files via upload

---
+++ set_methods.py
@@ -0,0 +1,20 @@
+# s = {1, 2, 3, 4}
+# s2 = {9, 8, 7, 6}
+# s.update([4, 5, 6], s2)
+# s.remove(5)
+# s.discard(5)
+# print(s)
+
+# s1 = {1, 2, 3}
+# s2 = {2, 3, 4}
+# s3 = {3, 4, 5}
+#
+# s4 = s1.intersection(s2, s3)
+# s5 = s1.difference(s2)
+# s6 = s2.difference(s1, s3)
+#
+# s7 = s1.symmetric_difference(s2)
+# print(s7)
+
+
+
